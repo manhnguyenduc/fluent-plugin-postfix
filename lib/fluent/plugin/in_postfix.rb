99ca10ea13895f8b11ee3f14cf5dfbec0107b2bf
Tieu Vu <manhnguyenduc@vccorp.vn>
2015-05-22 14:06:26 +0700

    edit handler bounce only

--- lib/fluent/plugin/in_postfix.rb
+++ lib/fluent/plugin/in_postfix.rb
@@ -86,7 +86,7 @@ class PostfixLog
 
   def merge(type, time, record)
     case type
-    when :sent, :deferred, :bounced
+    when :bounced
       @tos[record["to"]] = record
     when :from
       @from = record["from"];
